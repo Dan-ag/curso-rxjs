6820322dcfb09788d917c0d52ecc954dac2d4b66
Danilo O.R <daorozco@saludelectronica.com>
2020-03-31 23:02:33 -0500

    5-fromEvent

---
+++ src/Observables/5-fromEvent.ts
@@ -0,0 +1,24 @@
+import { fromEvent } from 'rxjs';
+
+
+/**
+ * Eventos del dom
+ */
+
+
+
+const src1$ = fromEvent<MouseEvent>( document, 'click' );
+const src2$ = fromEvent<KeyboardEvent>( document, 'keyup' );
+
+const observer = {
+  next: val => console.log('next', val),
+}
+
+src1$.subscribe( ({x, y}) => {
+  console.log( x );
+  console.log( y );
+} )
+
+src2$.subscribe( evento => {
+  console.log( evento.key );
+} )
