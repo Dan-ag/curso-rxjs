e218b55581ec50143fc08aa1c043350a66bd6cfc
Danilo O.R <daorozco@saludelectronica.com>
2020-04-04 14:46:53 -0500

    12-distincUntilChanged

--- src/Observables/operadores/11-distinc.ts
+++ src/Observables/operadores/11-distinc.ts
@@ -1,17 +1,17 @@
 import { of, from } from 'rxjs';
 import { distinct } from 'rxjs/operators';
 
-const numeros$ = of<number | string>( 1, '1', 1, 2, 3, 1, 5, 3, 4, 7 )
+const numeros$ = of<number | string>( 1, '1', 1, 2, 3, 1, 5, 3, 4, 7 );
 
 
 numeros$.pipe(
   distinct() // ===
 )
-  .subscribe( console.log )
+  .subscribe( console.log );
 
 
 interface Personaje {
-  nombre: string
+  nombre: string;
 }
 
 const personajes: Personaje[] = [
@@ -19,10 +19,19 @@ const personajes: Personaje[] = [
     nombre: 'Megaman'
   },
   {
+    nombre: 'Megaman'
+  },
+  {
     nombre: 'Zero'
   },
   {
+    nombre: 'x'
+  },
+  {
     nombre: 'Megaman'
+  },
+  {
+    nombre: 'x'
   },
 ];
 
@@ -31,4 +40,4 @@ from( personajes )
   .pipe(
     distinct( p => p.nombre )
   )
-  .subscribe( console.log )
+  .subscribe( console.log );
