67bc9313176c0e6c86397eac21ebc2a14fab4e38
Danilo O.R <daorozco@saludelectronica.com>
2020-04-05 00:37:30 -0500

    2-mergeMap

---
+++ src/Observables/operadores-transformacion/2-mergeMap.ts
@@ -0,0 +1,31 @@
+import { mergeMap, take, map, mergeMapTo, takeUntil } from 'rxjs/operators';
+import { interval, of, Observable, fromEvent } from 'rxjs';
+
+
+
+
+
+const letras$ = of<string>( 'a', 'b', 'c' );
+
+
+letras$.pipe(
+  mergeMap( letra => interval( 1000 ).pipe(
+    map( i => letra + 1 ),
+    take( 3 )
+  ) )
+)
+  // .subscribe( {
+  //   next: val => console.log( 'next: ', val ),
+  //   complete: () => console.log( 'Complete' )
+  // } );
+
+const mouseDown$ = fromEvent( document, 'mousedown' )
+const mouseup$ = fromEvent( document, 'mouseup' )
+const interval$ = interval()
+
+mouseDown$.pipe(
+  mergeMap( () => interval$.pipe(
+    takeUntil( mouseup$ )
+  ) )
+)
+.subscribe( console.log )
