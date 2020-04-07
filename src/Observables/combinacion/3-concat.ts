3f467bb2e71a01374bd86cc464796ca7a535e7b5
Danilo O.R <daorozco@saludelectronica.com>
2020-04-07 16:16:23 -0500

    5e-combinar

--- src/Observables/combinacion/3-concat.ts
+++ src/Observables/combinacion/3-concat.ts
@@ -6,6 +6,6 @@ const interval$ = interval( 1000 )
 
 concat(
   interval$.pipe( take(3)),
-  interval$.pipe( take( 2 ) ),
+  interval$.pipe( take(2) ),
   of(1)
 ).subscribe( console.log )
--- src/Observables/combinacion/5-combineLatest.ts
+++ src/Observables/combinacion/5-combineLatest.ts
@@ -1,34 +1,54 @@
-import { fromEvent, merge, Observable, combineLatest } from 'rxjs';
-import { pluck } from 'rxjs/operators';
+import { interval, timer, combineLatest } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+/**
+ * Ejercicio: Combinar ambos observables (letras$, numeros$)
+ * para que las emisiones sean la concatenación de los últimos
+ * valores emitidos
+ */
 
-// const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup' );
-// const click$ = fromEvent<MouseEvent>( document, 'click' );
+//  Ejemplo de la tada esperada:
+// a1
+// a2
+// b2
+// b3
+// c3
+// c4
+// d4
+// d5
+// e5
 
-// combineLatest(
-//   keyup$.pipe( pluck<KeyboardEvent,string>( 'type' ) ),
-//   click$.pipe( pluck<MouseEvent,string>( 'type' ) )
-// ).subscribe( console.log );
+( () => {
 
-const input1 = document.createElement( 'input' )
-const input2 = document.createElement( 'input' )
+  const letras = [ 'a', 'b', 'c', 'd', 'e' ];
+  const numeros = [ 1, 2, 3, 4, 5 ];
 
-input1.placeholder = 'email@g.com'
+  // Emite letras cada segundo
+  const letras$ = interval( 1000 ).pipe(
+    map( i => letras[ i ] ),
+    take( letras.length )
+  );
 
-input2.placeholder = '***************'
-input2.type = 'password'
+  // Emite numeros del 1 al 5 cada segundo, pero tiene un delay inicial
+  // de 500 milésimas 
+  const numeros$ = timer( 500, 1000 ).pipe(
+    map( i => numeros[ i ] ),
+    take( numeros.length )
+  );
 
-document.querySelector( 'body' ).append( input1, input2 );
+  // ========================================
+  // Empezar a codificar aquí abajo
+  // Nota, el subscribe debe de ser así
+  // .subscribe( console.log )
+  // Es decir, la salida en el subscribe debe 
+  // de estar procesada en su totalidad
+  // ========================================
 
+  combineLatest( letras$, numeros$ )
+    .pipe(
+      map( val => val.join( '' ) ),
+      // map( ([a,b]) => a + b )
+    )
+    .subscribe( console.log );
 
-// Helper
-const getInputStream = ( elem: HTMLElement ) =>
-  fromEvent<KeyboardEvent>( elem, 'keyup' )
-  .pipe(
-    pluck<KeyboardEvent, string>( 'target', 'value' )
-  )
-
-combineLatest( 
-  getInputStream( input1 ),
-  getInputStream( input2 )
-).subscribe( console.log )
+} )();
 
