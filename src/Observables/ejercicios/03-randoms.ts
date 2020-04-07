ba2b768e53fddf1a1ca12c18b49a223b1ba14d1b
Danilo O.R <daorozco@saludelectronica.com>
2020-04-07 15:17:02 -0500

    3e-random

--- src/Observables/ejercicios/03-randoms.ts
+++ src/Observables/ejercicios/03-randoms.ts
@@ -1,4 +1,4 @@
-import { interval } from 'rxjs';
+import { interval, Subject } from 'rxjs';
 import { take, map } from 'rxjs/operators';
 /**
  * Ejercicio: Realizar que los dos observables finales, 
@@ -7,27 +7,22 @@ import { take, map } from 'rxjs/operators';
  * Tip: Hot Observable? subjects?
  */
 
-(() =>{
+( () => {
 
   // == NO TOCAR este bloque ====================
-  const reloj$ = interval(1000).pipe(
-    take(5),
-    map( val => Math.round(Math.random() * 100) )
+  const reloj$ = interval( 1000 ).pipe(
+    take( 5 ),
+    map( val => Math.round( Math.random() * 100 ) )
   );
   // No tocar la creación del observable
   // ============================================
 
-    
-  
-  
   // Estos dos observables deben de emitir exactamente los mismos valores
-  reloj$.subscribe( val => console.log('obs1', val) );
-  reloj$.subscribe( val => console.log('obs2', val) );
+  const subjects$ = new Subject();
+  reloj$.subscribe( subjects$ );
 
+  subjects$.subscribe( val => console.log( 'obs1', val ) );
+  subjects$.subscribe( val => console.log( 'obs2', val ) );
 
+} )();
 
-
-
-})();
-
-		
